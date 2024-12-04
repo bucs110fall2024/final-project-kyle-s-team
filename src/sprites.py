1645075898c19b9d24de4a21f4881e802125226e
Kyle Godzki <kyledg01281274@gmail.com>
2024-12-04 13:43:35 -0500

    cleaned up tilemap

--- src/sprites.py
+++ src/sprites.py
@@ -3,14 +3,11 @@ import pygame
 class SpriteCollision(pygame.sprite.Sprite):
     def __init__(self, pos, surface, groups):
         super().__init__(groups)
-        # self.image = pygame.Surface(size)
-        # self.image.fill("blue")
         self.image = surface
-        self.rect = self.image.get_rect(center = pos)
-        pygame.draw.rect(self.image, (255, 0, 0), self.rect, 2)
+        self.rect = self.image.get_rect(topleft = pos)
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, pos, surface, groups):
         super().__init__(groups)
         self.image = surface
-        self.rect = self.image.get_rect(center = pos)
+        self.rect = self.image.get_rect(topleft = pos)
