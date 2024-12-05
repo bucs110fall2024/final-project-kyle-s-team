e7652981a5450d4b79c4e163a4a779ba0c8ab936
Kyle Godzki <kyledg01281274@gmail.com>
2024-12-04 21:25:57 -0500

    gun rotates based on mouse

--- src/gun.py
+++ src/gun.py
@@ -4,7 +4,7 @@ class Gun(pygame.sprite.Sprite):
     def __init__(self, player, groups):
         self.player = player
         self.distance = 140
-        self.player_direction = pygame.Vector2(1, 0)
+        self.player_direction = pygame.Vector2(0, 1)
 
         super().__init__(groups)
         self.gun_surface = pygame.image.load("assets/Gun/gun.png").convert_alpha()
@@ -12,8 +12,19 @@ class Gun(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (150, 150))
         self.rect = self.image.get_rect(center = self.player.rect.center + self.player_direction * self.distance)
 
+    def rotate(self):
+        """
+        Rotate gun based on direction needed to shoot
+        """
+        WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+        mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+        player_pos = pygame.Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+        #Vector math!
+        self.player_direction = (mouse_pos - player_pos).normalize()
+        
 
     def update(self, _):
+        self.rotate()
         self.rect.center = self.player.rect.center + self.player_direction * self.distance
 
     def bullet(self):
@@ -24,7 +35,3 @@ class Gun(pygame.sprite.Sprite):
         spawns within the player's hitbox.
         """
 
-    def rotate(self):
-        """
-        Rotate gun based on direction needed to shoot
-        """
