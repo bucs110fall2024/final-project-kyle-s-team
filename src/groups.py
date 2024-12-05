69305de0fcf4e2fd771c3e0a76f16c55b8b02961
Kyle Godzki <kyledg01281274@gmail.com>
2024-12-05 12:52:07 -0500

    added camer constraints

--- src/groups.py
+++ src/groups.py
@@ -5,11 +5,20 @@ class AllSprites(pygame.sprite.Group):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
         self.offset = pygame.Vector2()
+
+        #Level boundaries
+        self.level_width = 1440
+        self.level_height = 960
     
     #Camera
     def draw(self, target_pos):
-        WINDOW_HEIGHT = 720
-        self.offset.x = -(target_pos[0] - WINDOW_HEIGHT / 2)
+        WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+
+        self.offset.x = -(target_pos[0] - WINDOW_WIDTH / 2)
         self.offset.y = -(target_pos[1] - WINDOW_HEIGHT / 2)
+
+        self.offset.x = max(min(self.offset.x, 0), -(self.level_width - WINDOW_WIDTH))
+        self.offset.y = max(min(self.offset.y, 0), -(self.level_height - WINDOW_HEIGHT))
+
         for sprite in self:
             self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
