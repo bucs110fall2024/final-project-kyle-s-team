4ed655ba7db6ac1c6f75e89f6928ddaced1abb66
Kyle Godzki <kyledg01281274@gmail.com>
2024-12-04 21:57:44 -0500

    gun sprite also rotates

--- src/gun.py
+++ src/gun.py
@@ -1,4 +1,5 @@
 import pygame
+from math import atan2, degrees
 
 class Gun(pygame.sprite.Sprite):
     def __init__(self, player, groups):
@@ -11,10 +12,12 @@ class Gun(pygame.sprite.Sprite):
         self.image = self.gun_surface
         self.image = pygame.transform.scale(self.image, (150, 150))
         self.rect = self.image.get_rect(center = self.player.rect.center + self.player_direction * self.distance)
-
+        self.current_angle = None
+        #Adjust for sprite offsets
+        self.offset = pygame.Vector2(15, 15)
     def rotate(self):
         """
-        Rotate gun based on direction needed to shoot
+        Rotate gun object based on direction needed to shoot
         """
         WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
         mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
@@ -22,9 +25,24 @@ class Gun(pygame.sprite.Sprite):
         #Vector math!
         self.player_direction = (mouse_pos - player_pos).normalize()
         
+    def rotate_sprite(self):
+        """
+        Rotate gun sprite based on direction needed to shoot
+        Use trig to find how to properly rotate the sprite, must covert to degrees as it's in radians
+        """
 
+        angle = -degrees(atan2(self.player_direction.y, self.player_direction.x))
+        
+        # Update the sprite only if the angle has changed
+        if angle != self.current_angle:
+            self.current_angle = abs(angle)
+            self.image = pygame.transform.rotozoom(self.gun_surface, angle, .1)
+            self.rect = self.image.get_rect(center=self.player.rect.center + self.player_direction * self.distance + self.offset)
+
+                                        
     def update(self, _):
         self.rotate()
+        self.rotate_sprite()
         self.rect.center = self.player.rect.center + self.player_direction * self.distance
 
     def bullet(self):
