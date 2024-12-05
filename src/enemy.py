0dc86e06ac4feff7d30c9506f92dc6ed3c50d68a
Kyle Godzki <kyledg01281274@gmail.com>
2024-12-05 09:47:03 -0500

    Enemies can walk and collide with walls

--- src/enemy.py
+++ src/enemy.py
@@ -13,14 +13,48 @@ class Enemy(pygame.sprite.Sprite):
         self.animation_speed = 6
 
         self.rect = self.image.get_rect(center = pos)
-        self.hitbox_rect = self.rect.inflate(-20, -40)
+        self.hitbox_rect = self.rect.inflate(-50, -80)
         self.collision_sprites = collision_sprites
         self.direction = pygame.Vector2()
-        self.speed = 300
+        self.speed = 200
 
-    def move(self):
-        """
-        Move enemy towards player
-        Create player position and enemy position variables to get both,
-        using vectors I'll determine which direction the enemy should move
-        """
+    def animate(self, dt):
+        self.frame_index += self.animation_speed * dt
+        self.image = self.frames[int(self.frame_index) % len(self.frames)]
+
+    def move(self, dt):
+        """""
+        Moves the enemy, gets it direction and updates rect position
+        Includes collision logic
+        """""
+        player_pos = pygame.Vector2(self.player.rect.center)
+        enemy_pos = pygame.Vector2(self.rect.center)
+        direction = player_pos - enemy_pos
+        if direction.length() > 0:
+            self.direction = direction.normalize()  #Vector math that allows for consitent movement
+        else:
+            self.direction = pygame.Vector2(0, 0)
+
+        # self.rect.center += self.direction * self.speed * dt
+        self.rect.x += self.direction.x * self.speed * dt
+        self.collision("horizontal")
+        self.rect.y += self.direction.y * self.speed * dt
+        self.collision("vertical")
+
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.rect):
+                if direction == "horizontal":
+                    if self.direction.x > 0:
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0:
+                        self.rect.left = sprite.rect.right
+                elif direction == "vertical":
+                    if self.direction.y > 0:
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0:
+                        self.rect.top = sprite.rect.bottom
+    
+    def update(self, dt):
+        self.move(dt)
+        self.animate(dt)
